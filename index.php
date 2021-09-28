3c82b80ed42e48bd19eed16c67fd69dd96188b73
Gatis Ķevans <gatis.kevans@gmail.com>
2021-09-28 16:58:45 +0300

    if user enters a number it will return location Riga

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 include 'Search/locationForm.php';
 
 if (isset($_GET['submit'])) {
-    if ($_GET['location'] === '') {
+    if ($_GET['location'] === '' || is_numeric($_GET['location'])) {
         $_GET['location'] = 'Riga';
     }
     include 'Search/table.php';
